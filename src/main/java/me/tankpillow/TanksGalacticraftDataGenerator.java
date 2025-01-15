94de025384a179be96b428a693a17b63c8f51980
tankpillow <tankpillow@gmail.com>
2025-01-14 23:48:59 -0500

    Added project files

---
+++ src/main/java/me/tankpillow/TanksGalacticraftDataGenerator.java
@@ -0,0 +1,13 @@
+package me.tankpillow;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TanksGalacticraftDataGenerator implements DataGeneratorEntrypoint 
+{
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) 
+	{
+
+	}
+}
